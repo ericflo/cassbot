737b3380126213ea56618d1113998d7b0b51e4f6
Brandon Williams <brandon@faltering.com>
2009-12-18 14:53:56 -0600

    Commands are more extensible, allow commands via private msg

--- bot.py
+++ bot.py
@@ -6,12 +6,12 @@ import logging
 import logging.handlers
 
 from twisted.words.protocols import irc
-from twisted.internet import reactor, protocol
+from twisted.internet import reactor, defer, protocol
 from twisted.web.client import getPage
+from twisted.web.error import Error
 
 TICKET_RE = re.compile(r'#(\d+)')
 COMMIT_RE = re.compile(r'r(\d+)')
-BUILD_RE = re.compile(r'build ([\w\-]+)')
 
 LOG_BLACKLIST = [
     'evn',
@@ -40,56 +40,66 @@ class CassBot(irc.IRCClient):
     def joined(self, channel):
         print "Joined %s." % (channel,)
     
-    def ticketCallback(self, user, msg):
+    def checklinks(self, user, msg):
         match = TICKET_RE.search(msg)
-        if not match:
-            return
-        ticket = int(match.group(1))
-        url = 'http://issues.apache.org/jira/browse/CASSANDRA-%d' % (ticket,)
-        self.msg(self.factory.channel, url)
+        if match:
+            ticket = int(match.group(1))
+            url = 'http://issues.apache.org/jira/browse/CASSANDRA-%d' % (ticket,)
+            self.msg(self.factory.channel, url)
+        else:
+            match = COMMIT_RE.search(msg)
+            if match:
+                commit = int(match.group(1))
+                url = 'http://svn.apache.org/viewvc?view=rev&revision=%d' % (commit,)
+                self.msg(self.factory.channel, url)
     
-    def logsCallback(self, user, msg):
-        if self.nickname.lower() not in msg.lower():
-            return
-        if 'logs' not in msg.lower():
-            return
-        self.msg(self.factory.channel, 'http://www.eflorenzano.com/cassbot/')
+    def command_LOGS(self, user, channel, args):
+        self.msg(channel, 'http://www.eflorenzano.com/cassbot/')
 
-    def commitCallback(self, user, msg):
-        match = COMMIT_RE.search(msg)
-        if not match:
+    @defer.inlineCallbacks
+    def command_BUILD(self, user, channel, args):
+        if not args[0]:
+            self.msg(channel, "usage: build <buildname>")
             return
-        commit = int(match.group(1))
-        url = 'http://svn.apache.org/viewvc?view=rev&revision=%d' % (commit,)
-        self.msg(self.factory.channel, url)
-
-    def buildCallback(self, user, msg):
-        if self.nickname.lower() not in msg.lower():
-            return
-
-        match = BUILD_RE.search(msg)
-        if not match:
-            return
-
-        build = match.group(1)
-        url = '%s/%s/polling?token=%s' % (BUILD_URL, build, BUILD_TOKEN)
-        user = user.split('!', 1)[0]
-
-        # Hudson returns a 404 even when this request succeeds :/
-        def queued(result):
-            self.msg(self.factory.channel, "%s: request sent!" % (user,))
-        dfr = getPage(url).addCallbacks(callback=queued, errback=queued)
+        url = '%s/%s/polling?token=%s' % (BUILD_URL, args[0], BUILD_TOKEN)
+        msg = "request sent!"
+        try:
+            res = yield getPage(url)
+        except Error, e:
+            # Hudson returns a 404 even when this request succeeds :/
+            if e.status == '404':
+                pass
+            else:
+                msg = str(e)
+        if user != channel:
+            msg = user + ': ' + msg
+        self.msg(channel, msg)
     
     def privmsg(self, user, channel, msg):
-        if not user:
+        user = user.split('!', 1)[0]
+        if msg.startswith("%s" % self.nickname):
+            msg = msg[len("%s" % self.nickname):]
+            msg = msg.lstrip(';: ')
+        elif channel == self.nickname:
+            channel = user
+        else:
+            if user not in LOG_BLACKLIST:
+                logging.info('<' + user + '> ' + msg)
+            self.checklinks(user, msg)
             return
-        if not user.split('|')[0].rstrip('_') in LOG_BLACKLIST:
-            logging.info('<' + user.split('!')[0] + '> ' + msg)
-        self.ticketCallback(user, msg)
-        self.logsCallback(user, msg)
-        self.commitCallback(user, msg)
-        self.buildCallback(user, msg)
-
+        parts = msg.split(' ', 1)
+        if len(parts) == 1:
+            parts = parts + ['']
+        cmd, args = parts
+        meth = getattr(self, 'command_' + cmd.upper(), None)
+        if meth:
+            try:
+                meth(user, channel, args)
+            except Exception, e:
+                print "Exception in %s: %s" % (cmd, e)
+                self.msg(channel, "Ah crap, I got an exception :(")
+        else:
+            self.msg(channel, "Unknown command: %s" % cmd)
 
 class CassBotFactory(protocol.ReconnectingClientFactory):
     protocol = CassBot
